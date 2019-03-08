a3e6f1bdc9f934c2c512417d8c0084b5bff1bd5a
hendrike <andre.hendrike.alqarni@gmail.com>
2019-03-08 22:23:11 +0700

    initial comm

--- app/src/main/java/android/example/com/aplikasirempah/MainActivity.java
+++ app/src/main/java/android/example/com/aplikasirempah/MainActivity.java
@@ -1,5 +1,6 @@
 package android.example.com.aplikasirempah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,23 +9,27 @@ import android.widget.Button;
 import com.example.agug.aplikasirempah.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button keluar;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        //tombol keluar
-        keluar=(Button) findViewById(R.id.btnKeluar);
-        keluar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //memanggil Allert dialog keluar
-                showAlertDialog();
-            }
-        });
+        Button GoToNewActivity;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            GoToNewActivity = (Button)findViewById(R.id.button2);
+
+            GoToNewActivity.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    // Intent code for open new activity through intent.
+
+                    Intent intent = new Intent(MainActivity.this, registrasi.class);
+                    startActivity(intent);
+
+                }
+            });
+        }
     }
 
-    private void showAlertDialog() {
-    }
-}
--- app/src/main/java/com/example/agug/aplikasirempah/HalamanUser.java
+++ app/src/main/java/com/example/agug/aplikasirempah/HalamanUser.java
@@ -8,6 +8,6 @@ public class HalamanUser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_halaman_user);
+        setContentView(R.layout.activity_main);
     }
 }
