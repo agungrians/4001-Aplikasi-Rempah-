3dc721a911c53878b90029f836ce3856faf9da09
Agung Riansyah <riansyahagung64@gmail.com>
2019-03-21 14:25:29 +0700

    init commit

---
+++ app/src/main/java/android/example/com/aplikasirempah/MainActivity.java
@@ -0,0 +1,35 @@
+package android.example.com.aplikasirempah;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.agug.aplikasirempah.R;
+
+public class MainActivity extends AppCompatActivity {
+
+        Button GoToNewActivity;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            GoToNewActivity = (Button)findViewById(R.id.button2);
+
+            GoToNewActivity.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+
+                    // Intent code for open new activity through intent.
+
+                    Intent intent = new Intent(MainActivity.this, registrasi.class);
+                    startActivity(intent);
+
+                }
+            });
+        }
+    }
+
