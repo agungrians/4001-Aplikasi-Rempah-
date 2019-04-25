e44bd4c4bd5496c8edd74ac710f1397a4298984c
unknown <kevinsimamora49@gmail.com>
2019-04-25 21:37:53 +0700

    commit

---
+++ app/src/main/java/android/example/com/rempahpertiwi/activity_utama.java
@@ -0,0 +1,13 @@
+package android.example.com.rempahpertiwi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+    }
+}
